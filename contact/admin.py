849c9ad2a1fd4bf9f499ef20927621595d89aece
Diego <diegoalencar.jacober@gmail.ocm>
2023-07-29 10:49:31 -0300

    Script para gerar dados fakes

--- contact/admin.py
+++ contact/admin.py
@@ -12,8 +12,8 @@ class ContactAdmin(admin.ModelAdmin):
     #Pesquisas
     search_fields = ('id', 'first_name', 'last_name', 'phone', 'email')
     # Paginação
-    list_per_page = 1
-    list_max_show_all = 10
+    list_per_page = 10
+    list_max_show_all = 100
     #Editar sem entrar no registro
     list_editable = ('first_name', 'last_name',)
     #Link para o registro
