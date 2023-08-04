0b5c216da1bc19733967a82d289bb71e8db5e8c9
Diego Alencar Jacober <57761933898>
2023-08-04 16:43:26 -0300

    Inserindo Search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 def index(request):
@@ -23,4 +24,21 @@ def contact(request, contact_id):
     }
     
     return render(request, template_name='contact/contact.html', context=context)
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    
+    if search_value == '':
+        return redirect('contact:index')
+    
+    # https://docs.djangoproject.com/en/4.2/ref/models/querysets/#field-lookups
+    # filter(first_name__icontains=search_value, last_name__icontains=search_value) == AND
+    contacts = Contact.objects.filter(show=True).filter(Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value) |  Q(email__icontains=search_value)).order_by('id')[:10] # OR
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos',
+    }
+    
+    return render(request, template_name='contact/index.html', context=context)
 
